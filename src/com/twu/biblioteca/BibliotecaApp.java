43bb6d5d699138f291941e5cb023e7ce7ae3bd98
Gabrielle Campos <gcampos@thoughtworks.com>
2017-02-23 14:59:52 -0300

    List books including details

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,11 +1,28 @@
 package com.twu.biblioteca;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BibliotecaApp {
 
     public static String getWelcomeMessage(){
         return "Welcome!";
     }
+
+    public static Archive loadArchive(){
+        Book bible = new Book("Bible", "God", 0);
+        Book americanGods = new Book("American Gods", "Neil Gaiman", 2001);
+        List<Book> books = new ArrayList<>();
+        bible.checkOut();
+        books.add(bible);
+        books.add(americanGods);
+        return new Archive(books);
+    }
     public static void main(String[] args) {
+
+        Archive archive = loadArchive();
         System.out.println(getWelcomeMessage());
+        System.out.println(archive.listBooks());
+
     }
 }
